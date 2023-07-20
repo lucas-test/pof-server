d5180dc550dc0e5ad39b63f5b43119406655859d
jawaka345 <lu.k@p2p.legal>
2023-07-20 16:57:58 +0200

    test2

--- src/app.js
+++ src/app.js
@@ -14,34 +14,31 @@ app.get('/', (req, res) => {
     }
     console.log(max);
 
-        // Set the response headers
-    res.setHeader('Content-Type', 'text/csv');
-    res.setHeader('Content-Disposition', 'attachment; filename="data.csv"');
+       
 
-    // Send the command output as the response
-    res.send("test,lol");
-
-    // exec("./pop8query --title " + title + " --max " + max, (error, stdout, stderr) => {
-    //     if (error) {
-    //       console.error(`Error executing command: ${error.message}`);
-    //       res.send();
-    //       return;
-    //     }
-    //     if (stderr) {
-    //       console.error(`Command error: ${stderr}`);
-    //       res.send();
-    //       return;
-    //     }
+    exec("./pop8query --title " + title + " --max " + max, (error, stdout, stderr) => {
+        if (error) {
+          console.error(`Error executing command: ${error.message}`);
+          res.send();
+          return;
+        }
+        if (stderr) {
+          console.error(`Command error: ${stderr}`);
+          res.send();
+          return;
+        }
         
 
-    //     // Set the response headers
-    //     res.setHeader('Content-Type', 'text/csv');
-    //     res.setHeader('Content-Disposition', 'attachment; filename="data.csv"');
+        // Set the response headers
+        res.setHeader('Content-Type', 'text/csv');
+        res.setHeader('Content-Disposition', 'attachment; filename="data.csv"');
 
-    //     // Send the command output as the response
-    //     res.send(stdout);
+        // Send the command output as the response
+        const data = stdout;
+        console.log(data);
+        res.send(data);
 
-    //   });
+      });
 
 
 });
